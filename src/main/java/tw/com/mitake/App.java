55815526c93852e4f814ec6e4453f465c20f6188
kewang <cpckewang@gmail.com>
2016-07-09 22:19:46 +0800

    Add skeleton

---
+++ src/main/java/tw/com/mitake/App.java
@@ -0,0 +1,13 @@
+package tw.com.mitake;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
